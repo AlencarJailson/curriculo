9cad45faf9c6354e00e54d226e043c010fce27ae
Jailson Alencar <alencarjailson88@gmail.com>
2021-06-17 15:09:15 -0300

    Old script correction

--- resources/scripts/script.js
+++ resources/scripts/script.js
@@ -1,1 +1,9 @@
-document.getElementById('age').innerText = (new Date().getFullYear()- 1989);
+const getAge = () => {
+  let age =  (new Date().getFullYear() - 1988);
+  let month = new Date().getMonth(); 
+  if(month < 11){
+    age -= 1;
+  }
+  return age;
+}
+document.getElementById('age').innerText = getAge();
